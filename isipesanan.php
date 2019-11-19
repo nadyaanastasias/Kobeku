76f89d81fb37d87cc57b8e7d9a93b4f5ad4eb26a
Nadya Anastasia <18217020@std.stei.itb.ac.id>
2019-11-19 14:32:04 +0700

    isipesanan

--- isipesanan.php
+++ isipesanan.php
@@ -5,20 +5,23 @@
         $password = "";
         $database = "kobeku";
 
+        $data1 = $data; // put the contents of the file into a variable
+        $characters = json_decode($data); // decode the JSON feed
+        echo $characters[0]->name;
         // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $database);
+        //$conn = mysqli_connect($servername, $username, $password, $database);
 
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+        //if ($conn->connect_error) {
+        //    die("Connection failed: " . $conn->connect_error);
+        //}
         // echo "Connected successfully";
 
-        $json_array = json_decode($_POST["data"]);
-        mysqli_query($conn,"INSERT INTO kobeku (idMakanan,quantity) VALUES(".$json_array['id'].", ".$json_array['count'].")");
-        $result = mysqli_query($conn, $sql);
-        $pesanan = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        
-        $sql = 'SELECT namaMakanan FROM pesanan, isipesanan, makanan WHERE pesanan.idPesanan=isipesanan.idPesanan and isipesanan.idMakanan=makanan.idMakanan';
-        $result = mysqli_query($conn, $sql);
-        $makanan = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        //$json_array = json_decode(orderdata);
+
+        //mysqli_query($conn,"INSERT INTO pesanan (totalHarga) VALUES(".$json_array['price'].")");
+
+        //$result = mysqli_query($conn, $sql);
+
+        //$pesanan = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
 ?>
